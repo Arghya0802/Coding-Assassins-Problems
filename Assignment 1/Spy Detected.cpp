571ed871af12ec4c1108a7d886d08f20979af342
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-05-22 12:51:11 +0530

    Create Spy Detected.cpp

---
+++ Assignment 1/Spy Detected.cpp
@@ -0,0 +1,39 @@
+// Link:
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+void solve(int t)
+{
+    while(t--)
+    {
+        int n ; cin >> n ;
+        int arr[n] ;
+
+        for(int i = 0 ; i < n ; i++)  cin >> arr[i] ;
+
+        int freq[101] = {0} ;
+
+        for(int i = 0 ; i < n ; i++)  freq[arr[i]] += 1 ;
+
+        int ans = -1 ;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(freq[arr[i]] == 1)  ans = i + 1 ;
+        }
+
+        cout << ans << endl ;
+    }
+}
+
+int main()
+{
+    int t ;  cin >> t ;
+
+    solve(t) ;
+    return 0 ;
+}
+
+// Time Complexity:  O(N)
+// Space Complexity: O(1) [As we are using only a frequency array of size 101]

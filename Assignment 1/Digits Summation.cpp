98fe0d5e92bc59ec47937be96af6b99c1aec3073
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-05-22 18:41:54 +0530

    Create Digits Summation.cpp

---
+++ Assignment 1/Digits Summation.cpp
@@ -0,0 +1,23 @@
+// Link:  https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+
+#include<bits/stdc++.h>
+using namespace std ;
+using ll = long long int ;
+
+void solve(ll m , ll n)
+{
+    ll sum = m % 10 + n % 10 ;
+
+    cout << sum << endl ;
+}
+
+int main()
+{
+    ll m , n ;  cin >> m >> n ;
+
+    solve(m , n) ;
+    return 0 ;
+}
+
+// Time Complexity:  O(1)
+// Space Complexity: O(1)

67b4fc7a46a7b6b48f3e61380f4f0fca6750c19b
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-05-22 12:54:46 +0530

    Create Again Twenty Five.cpp

---
+++ Assignment 1/Again Twenty Five.cpp
@@ -0,0 +1,21 @@
+// Link: https://codeforces.com/problemset/problem/630/A
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+void solve(int n)
+{
+    cout << "25" << endl ;
+    return ;
+}
+
+int main() 
+{
+    int n ;  cin >> n ;
+
+    solve(n) ;
+    return 0 ;
+}
+
+// Time Complexity:  O(1)
+// Space Complexity: O(1)
